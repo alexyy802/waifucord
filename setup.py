e44ea47727cf39bbb9f27bffb0ca25468ed537b9
Monke On Da Git <82736662+VincentRPS@users.noreply.github.com>
2021-10-28 23:00:05 -0700

    Bump! Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = []
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 
-version = ''
+version = '2.5.6'
 with open('senpai/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
