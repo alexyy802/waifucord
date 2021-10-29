28892a4bbae7bdfc72b65224a8d3221ed7adf3e8
Monke On Da Git <82736662+VincentRPS@users.noreply.github.com>
2021-10-28 22:44:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 
 version = ''
-with open('discord/__init__.py') as f:
+with open('senpai/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not version:
